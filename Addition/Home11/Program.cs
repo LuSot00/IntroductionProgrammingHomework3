f46b8aa91e8420ba8db4fb50cf56f02a090a4e79
LyubovS <l.sotskova@gmail.com>
2023-07-03 00:23:05 +0300

    Add Home 11

---
+++ Addition/Home11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Home_11. Дана сторона квадрата. Найти его периметр.
+
+
+Console.WriteLine("Введите длину стороны квадрата: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int perimeter = a * 2;
+Console.WriteLine($"Приметр квадрата равен {perimeter}");
