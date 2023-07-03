da147b6949eb374cfa20aa9c6acd1ef750740ae1
LyubovS <l.sotskova@gmail.com>
2023-07-03 23:55:04 +0300

    Continuation of Home 12

--- AdditionHome12/Program.cs
+++ AdditionHome12/Program.cs
@@ -35,3 +35,66 @@ x3 = x3 * x;
 Console.WriteLine("в)" + x3);
 Console.WriteLine();
 
+int x4 = x;
+x4 = x4 * x4;
+x4 = x4 * x4;
+x4 = x4 * x4;
+Console.WriteLine("г)" + x4);
+Console.WriteLine();
+
+int x5 = x;
+x5 = x5 * x5 * x5;
+x5 = x5 * x5 * x5;
+Console.WriteLine("д)" + x5);
+Console.WriteLine();
+
+int x6 = x;
+x6 = x6 * x6;
+x6 = x6 * x6;
+x6 = x6 * x;
+x6 = x6 * x6;
+Console.WriteLine("е)" + x6);
+Console.WriteLine();
+
+int x7 = x;
+x7 = x7 * x7 * x7;
+x7 = x7 * x7;
+x7 = x7 * x7;
+x7 = x7 * x;
+Console.WriteLine("ж)" + x7);
+Console.WriteLine();
+
+int x8 = x;
+x8 = x8 * x8;
+x8 = x8 * x8;
+x8 = x8 * x;
+x8 = x8 * x8 * x8;
+Console.WriteLine("з)" + x8);
+Console.WriteLine();
+
+int x9 = x;
+x9 = x9 * x9 * x9;
+x9 = x9 * x9;
+x9 = x9 * x;
+x9 = x9 * x9 * x9;
+Console.WriteLine("и)" + x9);
+Console.WriteLine();
+
+int x10 = x;
+x10 = x10 * x10 * x10;
+x10 = x10 * x10;
+x10 = x10 * x;
+x10 = x10 * x10;
+x10 = x10 * x10;
+Console.WriteLine("к)" + x10);
+Console.WriteLine();
+
+decimal x11 = x;
+x11 = x11 * x11;
+x11 = x11 * x11;
+x11 = x11 * x11;
+x11 = x11 * x11;
+x11 = x11 * x11;
+x11 = x11 * x11;
+Console.WriteLine("л)" + x11);
+Console.WriteLine();
